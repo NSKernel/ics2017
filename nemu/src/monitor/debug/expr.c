3ed9b1e33fcd0a7355fe75e334ac4bafc3fd7456
tracer-ics2017 <tracer@njuics.org>
2017-09-20 02:10:25 -0700

    >  compile 161220171 nskernel Linux ubuntu 4.8.0-36-generic #36~16.04.1-Ubuntu SMP Sun Feb 5 09:39:41 UTC 2017 i686 i686 i686 GNU/Linux  02:10:25 up 10:20,  1 user,  load average: 0.35, 0.27, 0.24 d7057fef7900d8f065b147716ac1ab1644562322

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -388,7 +388,6 @@ uint32_t eval(int p, int q, bool *success) {
   }
   else if (*success) {
     domop = finddom(p, q);
-    printf("Find dom at %d\n", domop);
     
     if (domop == -1) {
       *success = false;
