4a1df9a871a193c58e0b3414472da41910db836d
tracer-ics2017 <tracer@njuics.org>
2017-10-09 23:27:00 -0700

    >  compile 161220171 nskernel Linux ubuntu 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:03:33 UTC 2017 i686 i686 i686 GNU/Linux  23:27:00 up 10:39,  1 user,  load average: 0.36, 0.49, 0.53 96c9fc6a3fea69973021078fdec2613950ab53a1

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -33,8 +33,9 @@ static inline make_DopHelper(SI) {
   op->type = OP_TYPE_IMM;
 
   op->simm = instr_fetch(eip, op->width);
+  printf("%d\n", op->simm);
   op->simm = ((op->simm << (8 * (4 - op->width))) >> (8 * (4 - op->width)));
-  
+  printf("%d\n", op->simm);
   
   rtl_li(&op->val, op->simm);
 
@@ -264,8 +265,6 @@ make_DHelper(a2O) {
 
 make_DHelper(J) {
   decode_op_SI(eip, id_dest, false);
-  printf("%d", id_dest->simm);
-  printf("%d", id_dest->simm + *eip);
   // the target address can be computed in the decode stage
   decoding.jmp_eip = id_dest->simm + *eip;
 }
