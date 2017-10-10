1d6c34d882c1058b32b3297481a1635413f36161
tracer-ics2017 <tracer@njuics.org>
2017-10-10 04:14:28 -0700

    >  compile 161220171 nskernel Linux ubuntu 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:03:33 UTC 2017 i686 i686 i686 GNU/Linux  04:14:28 up 13:23,  1 user,  load average: 0.06, 0.12, 0.09 625f90f3ec7b52899a356e0126818dbfa710

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -61,7 +61,7 @@ make_group(gp4,
 
   /* 0xff */
 make_group(gp5,
-    EX(inc), EX(dec), EMPTY, EMPTY,
+    EX(inc), EX(dec), EMPTY, EX(call),
     EMPTY, EMPTY, EX(push), EMPTY)
 
   /* 0x0f 0x01*/
