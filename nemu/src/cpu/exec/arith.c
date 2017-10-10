d4063097b3d6af128360f674681e8601cfdeb9a0
tracer-ics2017 <tracer@njuics.org>
2017-10-10 03:41:26 -0700

    >  compile 161220171 nskernel Linux ubuntu 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:03:33 UTC 2017 i686 i686 i686 GNU/Linux  03:41:26 up 12:50,  1 user,  load average: 0.38, 0.26, 0.15 fc579c545cb93f34b2b1cfd861286b03439c8d0

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -32,6 +32,10 @@ make_EHelper(cmp) {
   rtl_sub(&t0, &id_dest->val, &id_src->val);
   t1 = (t0 > id_dest->val);
   rtl_set_CF(&t1);
+  printf("dest = %d\n", id_dest->val);
+  printf("src = %d\n", id_src->val);
+  printf("src >> 31 = %d\n", id_src->val >> 31);
+  printf("t0 = %d\n", t0);
   t1 = (((id_dest->val < 0) == ((id_src->val >> 31) == 0)) && ((t0 < 0) != (id_dest->val < 0)));
   rtl_set_OF(&t1);
   rtl_update_ZFSF(&t0, 4);
