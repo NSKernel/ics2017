087ade37efe7281cad3e6372248e93ccc98710ab
tracer-ics2017 <tracer@njuics.org>
2017-10-12 04:06:28 -0700

    >  compile 161220171 nskernel Linux ubuntu 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:03:33 UTC 2017 i686 i686 i686 GNU/Linux  04:06:28 up 20:43,  1 user,  load average: 0.50, 0.27, 0.23 38b700776c132fd89041b1fe30d2f5f8df3d7be6

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -43,14 +43,14 @@ make_rtl_arith_logic(sar)
 make_rtl_arith_logic(slt)
 make_rtl_arith_logic(sltu)
 
-static inline void rtl_ror(rtlreg_t* dest, const rtlreg_t* src1, const rtlreg_t* src2, int width) {
+static inline void rtl_rol(rtlreg_t* dest, const rtlreg_t* src1, const rtlreg_t* src2, int width) {
   uint32_t src2eff = *src2 % (width * 8);
   uint32_t partlow = ((*src1 & (((uint32_t)(0xFFFFFFFF) >> (32 - width * 8)) >> (width * 8 - src2eff))) << (width * 8 - src2eff));
   uint32_t partup = ((*src1 & ((((uint32_t)(0xFFFFFFFF) >> (32 - width * 8)) >> (src2eff)) << src2eff)) >> (src2eff));
   *dest = partlow | partup;
 }
 
-static inline void rtl_rol(rtlreg_t* dest, const rtlreg_t* src1, const rtlreg_t* src2, int width) {
+static inline void rtl_ror(rtlreg_t* dest, const rtlreg_t* src1, const rtlreg_t* src2, int width) {
   uint32_t src2eff = *src2 % (width * 8);
   uint32_t partlow = ((*src1 & (((uint32_t)(0xFFFFFFFF) >> (32 - width * 8)) >> src2eff)) << (src2eff));
   uint32_t partup = ((*src1 & ((((uint32_t)(0xFFFFFFFF) >> (32 - width * 8)) >> (width * 8 - src2eff) << (width * 8 - src2eff)))) >> (width * 8 - src2eff));
