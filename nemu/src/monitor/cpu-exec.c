8d5d039c8d615f76b70d113a5332b6cb4a47b5ac
tracer-ics2017 <tracer@njuics.org>
2017-10-11 19:36:52 -0700

    >  compile 161220171 nskernel Linux ubuntu 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:03:33 UTC 2017 i686 i686 i686 GNU/Linux  19:36:52 up 17:38,  1 user,  load average: 0.40, 0.52, 0.60 cff56052df11f45bdf88fe664da915d5eb8e572

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -22,7 +22,7 @@ void cpu_exec(uint64_t n) {
   }
   nemu_state = NEMU_RUNNING;
 
-  bool print_flag = n < MAX_INSTR_TO_PRINT;
+  bool print_flag = true; //n < MAX_INSTR_TO_PRINT;
 
   for (; n > 0; n --) {
     /* Execute one instruction, including instruction fetch,
