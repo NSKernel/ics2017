afc33d4b08233a66224ddbab2c17b51056d855d6
161220171-Zhao Shixuan <shixuan.zhao@hotmail.com>
2017-10-14 00:21:41 +0800

    before starting pa3

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -2,6 +2,8 @@
 #include <x86.h>
 
 #define RTC_PORT 0x48   // Note that this is not standard
+#define KEY_REG 0x60
+#define KEY_STATUS 0x64
 static unsigned long boot_time;
 
 void _ioe_init() {
@@ -23,8 +25,10 @@ extern void* memcpy(void *, const void *, int);
 
 void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
   int i;
-  for (i = 0; i < _screen.width * _screen.height; i++) {
-    fb[i] = i;
+  int j;
+  for (i = y; i < y + h && i < _screen.height; i++) {
+    for (j = x; j < x + w && j < _screen.width; j++)
+      fb[i * _screen.width + j]=pixels[(i - y) * w + j - x];
   }
 }
 
@@ -32,5 +36,8 @@ void _draw_sync() {
 }
 
 int _read_key() {
+  if (inb(KEY_STATUS)) {
+    return inl(KEY_REG);
+  }
   return _KEY_NONE;
 }
