82f873a7ac6862f55695d6b1e4c21784821012ae
161220171-Zhao Shixuan <shixuan.zhao@hotmail.com>
2017-10-14 00:29:21 +0800

    Implemented loader

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -2,7 +2,11 @@
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
+extern void ramdisk_read(void *buf, off_t offset, size_t len);
+
+extern size_t get_ramdisk_size();
+
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  ramdisk_read((void*)0x4000000, 0, get_ramdisk_size());
   return (uintptr_t)DEFAULT_ENTRY;
 }
