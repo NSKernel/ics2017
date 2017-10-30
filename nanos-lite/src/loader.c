decb7952b9d997bd33c1e83e9359da9261eb3e88
tracer-ics2017 <tracer@njuics.org>
2017-10-30 10:55:36 +0800

    >  compile 161220171 nskernel Linux ubuntu 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:03:33 UTC 2017 i686 i686 i686 GNU/Linux  10:55:36 up 1 day, 13:44,  1 user,  load average: 0.25, 0.19, 0.26 f8d20a58814f53c14b7096f7598b44b50d01de

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,5 +1,10 @@
 #include "common.h"
 
+extern int fs_open(const char *pathname, int flags, int mode);
+extern ssize_t fs_read(int fd, void *buf, size_t len);
+extern int fs_close(int fd);
+extern size_t fs_filesz(int fd);
+
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
 extern void ramdisk_read(void *buf, off_t offset, size_t len);
@@ -9,8 +14,9 @@ extern uintptr_t fs_getdiskoffset(int fd);
 extern size_t get_ramdisk_size();
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  ramdisk_read((void*)0x4000000, 0, get_ramdisk_size());
-  Log("\n\nLoad program at 0x%08X\n\n", fs_getdiskoffset(fs_open(filename, 0, 0)) + (uintptr_t)DEFAULT_ENTRY);
+  int fd = fs_open(filename, 0, 0);
+  fs_read(fd, DEFAULT_ENTRY, fs_filesz(fd));
+  fs_close(fd);
   
   return fs_getdiskoffset(fs_open(filename, 0, 0)) + (uintptr_t)DEFAULT_ENTRY;
 }
