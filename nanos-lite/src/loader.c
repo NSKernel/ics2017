9295a1e3274579c776bcdbeceb5e02508a795778
tracer-ics2017 <tracer@njuics.org>
2017-10-30 09:06:01 +0800

    >  run 161220171 nskernel Linux ubuntu 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:03:33 UTC 2017 i686 i686 i686 GNU/Linux  09:06:01 up 1 day, 12:02,  1 user,  load average: 0.32, 0.39, 0.38 38d12453747c39741705f6b075bd903f85689509

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -10,6 +10,6 @@ extern size_t get_ramdisk_size();
 
 uintptr_t loader(_Protect *as, const char *filename) {
   ramdisk_read((void*)0x4000000, 0, get_ramdisk_size());
-  
+  Log("\n\nLoad program at 0x%08X\n\n", fs_getdiskoffset(fs_open(filename, 0, 0)) + (uintptr_t)DEFAULT_ENTRY);
   return fs_getdiskoffset(fs_open(filename, 0, 0)) + (uintptr_t)DEFAULT_ENTRY;
 }
