93130d85fce60a93e3b32f165e36c530c1a7ad9c
tracer-ics2017 <tracer@njuics.org>
2017-10-30 09:42:34 +0800

    >  compile 161220171 nskernel Linux ubuntu 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:03:33 UTC 2017 i686 i686 i686 GNU/Linux  09:42:34 up 1 day, 12:35,  1 user,  load average: 1.06, 0.54, 0.40 7a11296dd83be8e9f72349d672f11824a9363383

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -122,8 +122,8 @@ static bool make_token(char *e) {
         int substr_len = pmatch.rm_eo;
         int IndexInTokenStr = 0;
 
-        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
-            i, rules[i].regex, position, substr_len, substr_len, substr_start);
+        //Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
+        //    i, rules[i].regex, position, substr_len, substr_len, substr_start);
         position += substr_len;
 
         /* TODO: Now a new token is recognized with rules[i]. Add codes
