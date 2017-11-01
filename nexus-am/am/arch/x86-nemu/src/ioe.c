c71c647ca62e6c70fc549786b3d937602e55ad40
tracer-ics2017 <tracer@njuics.org>
2017-11-01 10:28:44 +0800

    >  run 161220171 nskernel Linux nsvm0 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:22 UTC 2017 i686 i686 i686 GNU/Linux  10:28:44 up  1:30,  1 user,  load average: 0.11, 0.24, 0.18 e9feb98f656e98c5be8b112b559a49923edd729

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -28,11 +28,14 @@ ssize_t _copy_pixle(const uint32_t *pixels, off_t offset, size_t len) {
   if (offset + len > _screen.width * _screen.height * 4) {
     written = _screen.width * _screen.height * 4 - offset;
   }
-  _putc('c');
   if (written > 0)
     memcpy(fb + offset, pixels, (ssize_t)written);
   else
+  {
     written = 0;
+    _putc('f');
+    _putc('\n');
+  }
   return written;
 }
 
