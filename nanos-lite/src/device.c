870e1b80900b95a72415d1f94e3dfb183d6318ef
tracer-ics2017 <tracer@njuics.org>
2017-11-01 11:12:23 +0800

    >  run 161220171 nskernel Linux nsvm0 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:22 UTC 2017 i686 i686 i686 GNU/Linux  11:12:23 up  2:11,  1 user,  load average: 0.20, 0.22, 0.23 4055dd91e6fed2d4b3a8b995f8f5fc298e5e0a65

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -7,13 +7,36 @@ extern _Screen _screen;
 extern ssize_t _copy_pixle(const uint32_t *pixels, off_t offset, size_t len);
 extern uint32_t* const fb;
 
+extern int _read_key();
+extern unsigned long _uptime();
+
 static const char *keyname[256] __attribute__((used)) = {
   [_KEY_NONE] = "NONE",
   _KEYS(NAME)
 };
 
 size_t events_read(void *buf, size_t len) {
-  return 0;
+  size_t retsize;
+  int key = _read_key();
+  char buffer[128];
+  bool down = false;
+  if (key & 0x8000) {
+    key ^= 0x8000;
+    down = true;
+  }
+  
+  if (key != _KEY_NONE) {
+    sprintf(buffer, "k%s %s\n", down ? "d" : "u", keyname[key]);
+    retsize = strlen(buffer) > len ? len : strlen(buffer);
+    memcpy(buf, buffer, retsize);
+    return retsize;
+  }
+  else {
+    sprintf(buffer, "t %lu\n", _uptime());
+    retsize = strlen(buffer) > len ? len : strlen(buffer);
+    memcpy(buf, buffer, retsize);
+    return retsize;
+  }
 }
 
 static char dispinfo[128] __attribute__((used));
@@ -24,7 +47,6 @@ ssize_t dispinfo_read(void *buf, off_t offset, size_t len) {
   for (; it < len && offset + it < 128; it++) {
     ((char *)buf)[it] = dispinfo[offset + it];
   }
-  Log("it = %d", it);
   return it;
 }
 
