c3444a8fa2f53535f182466c065c941a96791817
tracer-ics2017 <tracer@njuics.org>
2017-12-01 16:43:23 +0800

    >  compile 161220171 nskernel Linux nsvm0 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:22 UTC 2017 i686 i686 i686 GNU/Linux  16:43:23 up 13:10,  1 user,  load average: 0.69, 0.57, 0.39 54ddd3e75e915b3afcad5a7c978d7675a3f903f4

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -10,10 +10,12 @@ make_EHelper(movfromc) {
   if(id_dest->reg == 0) {
     rtl_sr(id_src->reg, id_src->width, &cpu.cr0);
     sprintf(id_src->str, "cr0");
+    Log("From cr0\n");
   }
   else if(id_dest->reg == 3) {
     rtl_sr(id_src->reg, id_src->width, &cpu.cr3);
     sprintf(id_src->str, "cr3");
+    Log("From cr3\n");
   }
   else
     panic("Unexpected control register at 0x%08X\n", cpu.eip);
@@ -22,10 +24,14 @@ make_EHelper(movfromc) {
 
 make_EHelper(movtoc) {
   sprintf(id_dest->str, "cr%d", id_dest->reg);
-  if(id_dest->reg == 0)
+  if(id_dest->reg == 0) {
     cpu.cr0 = id_src->val;
-  else if(id_dest->reg == 3)
+    Log("To cr0\n");
+  }
+  else if(id_dest->reg == 3) {
     cpu.cr3 = id_src->val;
+    Log("To cr3\n");
+  }
   else
     panic("Unexpected control register at 0x%08X\n", cpu.eip);
   print_asm_template2(mov);
