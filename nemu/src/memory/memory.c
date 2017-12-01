37bfd7b13f4194cf59d9a69689762d627ff8c23a
tracer-ics2017 <tracer@njuics.org>
2017-12-01 16:36:17 +0800

    >  compile 161220171 nskernel Linux nsvm0 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:22 UTC 2017 i686 i686 i686 GNU/Linux  16:36:17 up 13:03,  1 user,  load average: 0.50, 0.32, 0.29 75ae02907faf97878930d7667bc66bbb724abf

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -9,6 +9,24 @@
 
 uint8_t pmem[PMEM_SIZE];
 
+paddr_t page_translate(vaddr_t vaddr, bool is_write) {
+  uint32_t DIR = vaddr >> 22;
+  uint32_t PAGE = vaddr >> 12 & 0x003FF000;
+  uint32_t OFFSET = vaddr & 0x00000FFF;
+
+  uint32_t *PageTable = (uint32_t *)(((uint32_t *)cpu.cr3)[DIR] & 0xFFFFF000);
+  assert(((uint32_t *)cpu.cr3)[DIR] & 0x00000001); // Present
+  ((uint32_t *)cpu.cr3)[DIR] |= 0x00000020; // Set accessed
+  uint32_t PageTableEntry = PageTable[PAGE];
+  assert(PageTableEntry & 0x00000001); // Present
+  PageTable[PAGE] |= 0x00000020; // Set accessed
+  if (is_write) 
+      PageTable[PAGE] |= 0x00000040; // Set dirty
+  paddr_t PhysicalAddr = (PageTableEntry & 0xFFFFF000) + OFFSET;
+
+  return PhysicalAddr;
+}
+
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
@@ -26,9 +44,30 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+  uint32_t OFFSET = addr & 0x00000FFF;
+
+  if (OFFSET + len > 0x00001000) {
+    int len1 = 0x00001000 - OFFSET;
+    int len2 = len - len1;
+    uint32_t part1 = paddr_read(page_translate(addr, false), len1); // low len1 bytes
+    uint32_t part2 = paddr_read(page_translate(addr + len1, false), len2); // high len2 bytes
+    // stich together
+    return part1 + (part2 << (len1 * 8));
+  }
+
+  return paddr_read(page_translate(addr, false), len);
 }
 
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
-  paddr_write(addr, len, data);
+  uint32_t OFFSET = addr & 0x00000FFF;
+  
+  if (OFFSET + len > 0x00001000) {
+    int len1 = 0x00001000 - OFFSET;
+    int len2 = len - len1;
+    paddr_write(page_translate(addr, true), len1, data); // low len1 bytes
+    data >>= (len1 * 8);
+    paddr_write(page_translate(addr + len1, true), len2, data); // high len2 bytes
+  }
+
+  paddr_write(page_translate(addr, true), len, data);
 }
