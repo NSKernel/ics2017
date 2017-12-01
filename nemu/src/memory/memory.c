aef6b82b0f2669dc76e3376b27c24fc970ea76b9
tracer-ics2017 <tracer@njuics.org>
2017-12-02 00:32:10 +0800

    >  compile 161220171 nskernel Linux nsvm0 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:22 UTC 2017 i686 i686 i686 GNU/Linux  00:32:10 up 15:35,  1 user,  load average: 1.06, 0.69, 0.41 352915fa9f2d2747f083d582398557bf2fa3cae

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -32,22 +32,22 @@ paddr_t page_translate(vaddr_t vaddr, bool is_write) {
   uint32_t OFFSET = vaddr & 0x00000FFF;
 
   uint32_t PageTable = paddr_read(cpu.cr3 + 4 * DIR, 4) & 0xFFFFF000;
-  //Log("vaddr = 0x%08X", vaddr);
-  //Log("DIR = %d", DIR);
-  //Log("PAGE = %d", PAGE);
-  //Log("OFFSET = %d", OFFSET);
-  //Log("PageTable = 0x%08X", paddr_read(cpu.cr3 + 4 * DIR, 4) & 0xFFFFF000);
+  Log("vaddr = 0x%08X", vaddr);
+  Log("DIR = %d", DIR);
+  Log("PAGE = %d", PAGE);
+  Log("OFFSET = %d", OFFSET);
+  Log("PageTable = 0x%08X", paddr_read(cpu.cr3 + 4 * DIR, 4) & 0xFFFFF000);
   assert(paddr_read(cpu.cr3 + 4 * DIR, 4) & 0x00000001); // Present
   paddr_write(cpu.cr3 + 4 * DIR, 4, (paddr_read(cpu.cr3 + 4 * DIR, 4) | 0x00000020)); // Set accessed
   uint32_t PageTableEntry = paddr_read(PageTable + 4 * PAGE, 4);
-  //Log("PageTableEntry = 0x%08X\n", PageTableEntry);
+  Log("PageTableEntry = 0x%08X\n", PageTableEntry);
   assert(PageTableEntry & 0x00000001); // Present
   paddr_write(PageTable + 4 * PAGE, 4, (paddr_read(PageTable + 4 * PAGE, 4) | 0x00000020)); // Set accessed
   if (is_write) 
       paddr_write(PageTable + 4 * PAGE, 4, (paddr_read(PageTable + 4 * PAGE, 4) | 0x00000040)); // Set dirty
   paddr_t PhysicalAddr = paddr_read(PageTableEntry & 0xFFFFF000, 4) + OFFSET;
 
-  //Log("PhysicalAddr = 0x%08X", PhysicalAddr);
+  Log("PhysicalAddr = 0x%08X", PhysicalAddr);
 
   return PhysicalAddr;
 }
