33d07ae508fab3e374433e796fb53d2225cee1c9
tracer-ics2017 <tracer@njuics.org>
2017-12-05 01:15:29 +0800

    >  compile 161220171 nskernel Linux nsvm0 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:22 UTC 2017 i686 i686 i686 GNU/Linux  01:15:29 up  2:56,  1 user,  load average: 0.50, 0.46, 0.40 5bbcf92f6316c68df39b732d9b8cf2994c5d7a2b

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -37,14 +37,18 @@ paddr_t page_translate(vaddr_t vaddr, bool is_write) {
   //Log("PAGE = %d", PAGE);
   //Log("OFFSET = %d", OFFSET);
   //Log("PageTable = 0x%08X", paddr_read(cpu.cr3 + 4 * DIR, 4) & 0xFFFFF000);
-  if(!(paddr_read(cpu.cr3 + 4 * DIR, 4) & 0x00000001))
-      Log("FATAL: eip = 0x%08X at PD", cpu.eip);
+  if(!(paddr_read(cpu.cr3 + 4 * DIR, 4) & 0x00000001)) {
+    Log("FATAL: Virtual Address is 0x%08X", vaddr);
+    Log("FATAL: eip = 0x%08X at PD", cpu.eip);
+  }
   assert(paddr_read(cpu.cr3 + 4 * DIR, 4) & 0x00000001); // Present
   paddr_write(cpu.cr3 + 4 * DIR, 4, (paddr_read(cpu.cr3 + 4 * DIR, 4) | 0x00000020)); // Set accessed
   uint32_t PageTableEntry = paddr_read(PageTable + 4 * PAGE, 4);
   //Log("PageTableEntry = 0x%08X\n", PageTableEntry);
-  if(!(PageTableEntry & 0x00000001))
-      Log("FATAL: eip = 0x%08X at PT", cpu.eip);
+  if(!(PageTableEntry & 0x00000001)) {
+    Log("FATAL: Virtual Address is 0x%08X", vaddr);
+    Log("FATAL: eip = 0x%08X at PT", cpu.eip);
+  }
   assert(PageTableEntry & 0x00000001); // Present
   paddr_write(PageTable + 4 * PAGE, 4, (paddr_read(PageTable + 4 * PAGE, 4) | 0x00000020)); // Set accessed
   if (is_write) 
