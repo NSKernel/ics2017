900a450af9a6060b9dac7c3f3207a4b5fd0943e6
tracer-ics2017 <tracer@njuics.org>
2017-12-05 00:09:18 +0800

    >  compile 161220171 nskernel Linux nsvm0 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:22 UTC 2017 i686 i686 i686 GNU/Linux  00:09:18 up  1:50,  1 user,  load average: 0.27, 0.31, 0.41 35b464ef758b261d7d3bf1c28a7914a79b318d1

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -38,13 +38,13 @@ paddr_t page_translate(vaddr_t vaddr, bool is_write) {
   //Log("OFFSET = %d", OFFSET);
   //Log("PageTable = 0x%08X", paddr_read(cpu.cr3 + 4 * DIR, 4) & 0xFFFFF000);
   if(!(paddr_read(cpu.cr3 + 4 * DIR, 4) & 0x00000001))
-      Log("FATAL: eip = 0x%08X", cpu.eip);
+      Log("FATAL: eip = 0x%08X at PD", cpu.eip);
   assert(paddr_read(cpu.cr3 + 4 * DIR, 4) & 0x00000001); // Present
   paddr_write(cpu.cr3 + 4 * DIR, 4, (paddr_read(cpu.cr3 + 4 * DIR, 4) | 0x00000020)); // Set accessed
   uint32_t PageTableEntry = paddr_read(PageTable + 4 * PAGE, 4);
   //Log("PageTableEntry = 0x%08X\n", PageTableEntry);
   if(!(PageTableEntry & 0x00000001))
-      Log("FATAL: eip = 0x%08X", cpu.eip);
+      Log("FATAL: eip = 0x%08X at PT", cpu.eip);
   assert(PageTableEntry & 0x00000001); // Present
   paddr_write(PageTable + 4 * PAGE, 4, (paddr_read(PageTable + 4 * PAGE, 4) | 0x00000020)); // Set accessed
   if (is_write) 
