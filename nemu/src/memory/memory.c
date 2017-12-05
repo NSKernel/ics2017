c38281da3bd3ff8db5d4c707a7c96e2ea812ae31
tracer-ics2017 <tracer@njuics.org>
2017-12-05 13:21:08 +0800

    >  compile 161220171 nskernel Linux nsvm0 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:22 UTC 2017 i686 i686 i686 GNU/Linux  13:21:08 up  4:54,  1 user,  load average: 0.35, 0.75, 0.79 d9e92352323ddf777cf113b5c97620cabc333634

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -15,8 +15,10 @@ uint8_t pmem[PMEM_SIZE];
 uint32_t paddr_read(paddr_t addr, int len) {
   if (is_mmio(addr) != -1)
     return mmio_read(addr, len, is_mmio(addr));
-  else
+  else {
+    Log ("Good");
     return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+  }
 }
 
 void paddr_write(paddr_t addr, int len, uint32_t data) {
