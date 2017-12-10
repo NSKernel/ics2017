9c33b5ec79b548ccd73e0892025a69067cc6f8f7
tracer-ics2017 <tracer@njuics.org>
2017-12-10 16:48:37 +0800

    >  run 161220171 nskernel Linux nsvm0 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:22 UTC 2017 i686 i686 i686 GNU/Linux  16:48:38 up  8:32,  1 user,  load average: 1.28, 1.09, 1.10 f718c511d12c829123d2dcf6b087e28cc4a69dff

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,16 +4,15 @@ extern _RegSet* do_syscall(_RegSet *r);
 extern _RegSet* schedule(_RegSet *prev);
 
 static _RegSet* do_event(_Event e, _RegSet* r) {
-  Log("Event %d", e);
   switch (e.event) {
     case _EVENT_SYSCALL: 
       do_syscall(r);
       break;
     case _EVENT_TRAP:
-      Log("Trap by 0x81");
+      //Log("Trap by 0x81");
       return schedule(r);
     case _EVENT_IRQ_TIME:
-      Log("Trap by time");
+      //Log("Trap by time");
       return schedule(r);
     default: panic("Unhandled event ID = %d", e.event);
   }
